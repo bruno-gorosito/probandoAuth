22d72460e5ff82a331c8aefe105334390a233cdf
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-06-13 10:52:24 -0300

    con login hecho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Header } from '@/components/headerPaginaPrincipal'
 import { SignInButton } from '@/components/signInButton'
-import { SessionProvider, useSession } from 'next-auth/react'
+import { SessionProvider, signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect } from 'react'
@@ -16,6 +16,9 @@ export default function Home() {
     <>
     <Header />
     <main>
+      <button
+        onClick={() => signIn()}
+      >sassssss</button>
       <SignInButton />
       <Link href="/auth/register">Registrarme</Link>
     </main>
