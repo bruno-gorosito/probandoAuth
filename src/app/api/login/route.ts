b97672cedb5ea3ec3e58fdf0d0795d4e59ed5959
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-06-13 12:17:45 -0300

    con login hecho

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -12,23 +12,24 @@ interface RequestBody {
 
 export async function POST(req: NextRequest) {
     // console.log(await req.json())
-    const body: RequestBody = await req.json();
-    cookies().set('body', body.email);
-    const user = await prisma.user.findUnique({
-        where: {
-            email: body.email,
-        }
-    })
-    console.log(user)
+    return NextResponse.json({msg: 'LLegué al post'})
+    // const body: RequestBody = await req.json();
+    // cookies().set('body', body.email);
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         email: body.email,
+    //     }
+    // })
+    // console.log(user)
 
-    if (user && (await bcrypt.compare(body.password, user.password))){
-        const {password, ...userWithoutPass} = user;
-        const accessToken = signJwtAccessToken(userWithoutPass);
-        const result = {
-            ...userWithoutPass,
-            accessToken
-        }
-        // cookies().set('token', accessToken)
-        return NextResponse.json(result)
-    } else return NextResponse.json(null)
+    // if (user && (await bcrypt.compare(body.password, user.password))){
+    //     const {password, ...userWithoutPass} = user;
+    //     const accessToken = signJwtAccessToken(userWithoutPass);
+    //     const result = {
+    //         ...userWithoutPass,
+    //         accessToken
+    //     }
+    //     // cookies().set('token', accessToken)
+    //     return NextResponse.json(result)
+    // } else return NextResponse.json(null)
 }
